04422585208f917807a4f777cba16ba6c82f77b6
Nick Olay <vast87@mail.ru>
2014-09-14 15:59:50 +0400

    Add index.erb, new.erb

--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
 require 'rubygems'
 require 'sinatra'
-require 'sinatra/reload'
+require 'sinatra/reloader'
 
 get '/' do
   erb :index
+end
+
+get '/new' do
+	erb :new
+end
+
+post '/new' do
+  @post = params[:post]
+
+  redirect to '/'
 end
