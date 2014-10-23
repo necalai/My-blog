482ccce7184355176aeaccf5660c000b8c81bb04
Nick Olay <vast87@mail.ru>
2014-10-23 19:36:07 +0400

    Small Refactoring.

--- app.rb
+++ app.rb
@@ -39,12 +39,10 @@ post '/new' do
 end
 
 get '/details/:id' do
-	def func
-
+	def func 
 		@post = Post.find(params[:id])	
 		c = Comment.where("post_id = #{@post.id}")
 		@comments = c.order('created_at DESC')
-
 	end
 
 	func
